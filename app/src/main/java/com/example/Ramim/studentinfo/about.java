1962e5693efc1bc107514a22288cda47e5e632b6
ramimhossain12 <ramimhossain6242@gmail.com>
2021-11-25 22:13:11 +0600

    about update

--- app/src/main/java/com/example/Ramim/studentinfo/about.java
+++ app/src/main/java/com/example/Ramim/studentinfo/about.java
@@ -1,12 +1,18 @@
 package com.example.Ramim.studentinfo;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class about extends AppCompatActivity {
+
+    private ImageView f,t,i,g,l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,49 @@ public class about extends AppCompatActivity {
         actionBar.setTitle("About Us");
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        f = findViewById(R.id.facebookID);
+        t = findViewById(R.id.twID);
+        i = findViewById(R.id.instagramID);
+        g = findViewById(R.id.gitID);
+        l = findViewById(R.id.linkedinID);
+
+        f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.facebook.com/hartless.hirdoy/");
+            }
+        });
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.instagram.com/ramim__hossain/");
+            }
+        });
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://twitter.com/RamimHo92163398");
+            }
+        });
+        g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://github.com/ramimhossain12");
+            }
+        });
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.linkedin.com/in/ramim-hossain-190ab217a/");
+            }
+        });
+
+    }
+
+    private void gotoUrl(String s) {
+
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 
     @Override
@@ -28,4 +77,8 @@ public class about extends AppCompatActivity {
         return true;
 
     }
+
+
+
+
 }
